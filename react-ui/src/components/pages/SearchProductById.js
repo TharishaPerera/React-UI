d0bab36b502d083d2634bd2ff28c0af417b6731a
Baxbie <tharishaperera@gmail.com>
2022-04-19 20:03:50 +0530

    added search by ID

--- react-ui/src/components/pages/SearchProductById.js
+++ react-ui/src/components/pages/SearchProductById.js
@@ -1,8 +1,41 @@
 import React, { useState } from 'react'
 import '../../App.css';
 import './SearchButtons.css';
+import ProductService from '../../services/ProductService';
 
 function SearchProductById() {
+    const [productId, setProductId] = useState();
+
+    const [productName, setName] = useState('');
+    const [productDescription, setDescription] = useState('');
+    const [productType, setType] = useState('');
+    const [unitPrice, setUnitPrice] = useState();
+    const [isAvailable, setAvailable] = useState('');
+    const [availableQuantity, setAvailableQuantity] = useState(0);
+    const [supplierId, setSupplierId] = useState();
+
+    const handleSearch = (productId) =>{
+        ProductService.getProductById(productId).then((response) =>{
+            setName(response.data.productName)
+            setDescription(response.data.productDescription)
+            setType(response.data.productType)
+            setUnitPrice(response.data.unitPrice)
+            setAvailable(response.data.isAvailable)
+            setAvailableQuantity(response.data.availableQuantity)
+            setSupplierId(response.data.supplierId)
+        })
+    }
+
+    const handleReset = () => {
+        setProductId(()=>"")
+        setName(()=>"")
+        setDescription(()=>"")
+        setType(()=>"")
+        setUnitPrice(()=>"")
+        setAvailable(()=>"")
+        setAvailableQuantity(()=>"")
+        setSupplierId(()=>"")
+    }
 
   return (
     <div className='container'>
@@ -10,10 +43,10 @@ function SearchProductById() {
             <div className="row g-3">
                 <form className="col-md-4 mt-5">
                     <label for="productId" className="form-label" >Enter Product ID:</label>
-                    <input type="number" min="1" className="form-control" id="productId" />                    
+                    <input type="number" min="1" className="form-control" id="productId" value={productId} onChange={(e) => setProductId(e.target.value)} />                    
                     <div className="mt-3">
-                        <button type="submit" className="btn btn-primary btn-sm search-btns">Search</button>
-                        <button type="reset" className="btn btn-secondary btn-sm search-btns">Reset</button>
+                        <button type="button" className="btn btn-primary btn-sm search-btns" onClick={() => handleSearch(productId)}>Search</button>
+                        <button type="reset" className="btn btn-secondary btn-sm search-btns" onClick={handleReset} >Reset</button>
                     </div>
                 </form>
             </div>
@@ -27,31 +60,31 @@ function SearchProductById() {
                 </div> */}
                 <div className="col-md-12">
                     <label for="productName" className="form-label">Product Name</label>
-                    <input type="text" className="form-control" id="productName" readonly />
+                    <input type="text" className="form-control" id="productName" value={productName} readonly />
                 </div>
                 <div className="col-12">
                     <label for="description" className="form-label">Description</label>
-                    <input type="text" className="form-control" id="description" readonly />
+                    <input type="text" className="form-control" id="description" value={productDescription} readonly />
                 </div>
                 <div className="col-6">
                     <label for="type" className="form-label">Product Type</label>
-                    <input type="text" className="form-control" id="type" readonly />
+                    <input type="text" className="form-control" id="type" value={productType} readonly />
                 </div>
                 <div className="col-md-6">
                     <label for="unitPrice" className="form-label">Unit Price</label>
-                    <input type="text" className="form-control" id="unitPrice" readonly />
+                    <input type="text" className="form-control" id="unitPrice" value={unitPrice} readonly />
                 </div>
                 <div className="col-md-4">
                     <label for="availablity" className="form-label">Availability</label>
-                    <input type="text" className="form-control" id="availablity" readonly />
+                    <input type="text" className="form-control" id="availablity" value={isAvailable} readonly />
                 </div>
                 <div className="col-md-4">
                     <label for="quantity" className="form-label">Quantity</label>
-                    <input type="text" className="form-control" id="quantity" readonly />
+                    <input type="text" className="form-control" id="quantity" value={availableQuantity} readonly />
                 </div>
                 <div className="col-md-4">
                     <label for="supplierId" className="form-label">Supplier ID</label>
-                    <input type="text" className="form-control" id="supplierId" readonly />
+                    <input type="text" className="form-control" id="supplierId" value={supplierId} readonly />
                 </div>
             </form>
         </div>
--- react-ui/src/components/pages/SearchSupplier.js
+++ react-ui/src/components/pages/SearchSupplier.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../App.css';
 import './SearchButtons.css';
+import SupplierService from '../../services/SupplierService';
 
 function SearchSupplier() {
+    const [supplierId, setSupplierId] = useState();
+
+    const [supplierName, setName] = useState('');
+    const [industry, setIndustry] = useState('');
+    const [supplierEmail, setEmail] = useState('');
+    const [supplierTelephone, setTelephone] = useState();
+    const [street, setStreet] = useState('');
+    const [city, setCity] = useState('');
+    const [country, setCountry] = useState('');
+    const [postalCode, setPostalCode] = useState();
+
+    const handleSearch = (supplierId) =>{
+        SupplierService.getSupplierById(supplierId).then((response) =>{
+            setName(response.data.supplierName)
+            setIndustry(response.data.industry)
+            setEmail(response.data.supplierEmail)
+            setTelephone(response.data.supplierTelephone)
+            setStreet(response.data.street)
+            setCity(response.data.city)
+            setCountry(response.data.country)
+            setPostalCode(response.data.postalCode)
+        })
+    }
+
+    const handleReset = () => {
+        setSupplierId(()=>"")
+        setName(() => "")
+        setIndustry(() => "")
+        setEmail(() => "")
+        setTelephone(() => "")
+        setStreet(() => "")
+        setCity(() => "")
+        setCountry(() => "")
+        setPostalCode(() => "")
+    }
+
     return (
         <div className='container'>
             <div classNameName="row mt-3">
                 <div className="row g-3">
                     <form className="col-md-4 mt-5">
                         <label for="productId" className="form-label">Enter Supplier ID:</label>
-                        <input type="number" min="1" className="form-control" id="productId" />
+                        <input type="number" min="1" className="form-control" id="productId" value={supplierId} onChange={(e) => setSupplierId(e.target.value)} />
                         <div className="mt-3">
-                            <button type="submit" className="btn btn-primary btn-sm search-btns">Search</button>
-                            <button type="reset" className="btn btn-secondary btn-sm search-btns">Reset</button>
+                            <button type="button" className="btn btn-primary btn-sm search-btns" onClick={() => handleSearch(supplierId)} >Search</button>
+                            <button type="reset" className="btn btn-secondary btn-sm search-btns" onClick={handleReset} >Reset</button>
                         </div>
                     </form>
                 </div>
@@ -21,36 +58,36 @@ function SearchSupplier() {
             <div className="mt-5" >
                 <form className="row g-3">
                     <div className="col-md-6">
-                        <label for="productName" className="form-label">Supplier Name</label>
-                        <input type="text" className="form-control" id="productName" readonly />
+                        <label for="supplierName" className="form-label">Supplier Name</label>
+                        <input type="text" className="form-control" id="supplierName" value={supplierName} readonly />
                     </div>
                     <div className="col-6">
-                        <label for="description" className="form-label">Industry</label>
-                        <input type="text" className="form-control" id="description" readonly />
+                        <label for="industry" className="form-label">Industry</label>
+                        <input type="text" className="form-control" id="industry" value={industry} readonly />
                     </div>
                     <div className="col-12">
-                        <label for="type" className="form-label">Supplier Email</label>
-                        <input type="text" className="form-control" id="type" readonly />
+                        <label for="supplierEmail" className="form-label">Supplier Email</label>
+                        <input type="text" className="form-control" id="supplierEmail" value={supplierEmail} readonly />
                     </div>
                     <div className="col-md-6">
-                        <label for="unitPrice" className="form-label">Supplier Telephone</label>
-                        <input type="number" className="form-control" id="unitPrice" readonly />
+                        <label for="supplierTelephone" className="form-label">Supplier Telephone</label>
+                        <input type="number" className="form-control" id="supplierTelephone" value={supplierTelephone} readonly />
                     </div>                    
                     <div className="col-md-6">
-                        <label for="productId" className="form-label">Postal Code</label>
-                        <input type="number" className="form-control" id="productId" readonly />
+                        <label for="postalCode" className="form-label">Postal Code</label>
+                        <input type="number" className="form-control" id="postalCode" value={postalCode} readonly />
                     </div>
                     <div className="col-md-4">
-                        <label for="availablity" className="form-label">Street</label>
-                        <input type="text" className="form-control" id="availablity" readonly />
+                        <label for="street" className="form-label">Street</label>
+                        <input type="text" className="form-control" id="street" value={street} readonly />
                     </div>
                     <div className="col-md-4">
-                        <label for="quantity" className="form-label">City</label>
-                        <input type="text" className="form-control" id="quantity" readonly />
+                        <label for="city" className="form-label">City</label>
+                        <input type="text" className="form-control" id="city" value={city} readonly />
                     </div>
                     <div className="col-md-4">
-                        <label for="supplierId" className="form-label">Country</label>
-                        <input type="text" className="form-control" id="supplierId" readonly />
+                        <label for="country" className="form-label">Country</label>
+                        <input type="text" className="form-control" id="country" value={country} readonly />
                     </div>
                 </form>
             </div>
